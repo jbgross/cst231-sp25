45f8da53bb08c9845e3a440f7ce38def9f676edf
jbgross <gross.joshua.b@gmail.com>
2025-01-27 11:16:42 -0800

    code from Monday, Jan 27

---
+++ src/wk02/Random_Sec1.java
@@ -0,0 +1,33 @@
+package wk02;
+
+import java.util.Random;
+
+public class Random_Sec1 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+        double z = 2.0;
+        System.out.println(x/y);
+        double a = x/y;
+//        x/y;
+
+
+        System.out.println(a);
+        System.out.println(x/a);
+        int q = 53489754;
+        double r = 3.14;
+        boolean s = true;
+        String t = "hello";
+        Random rand = new Random();
+        System.out.println(rand.nextInt());
+        System.out.println(rand.nextInt());
+        System.out.println(rand.nextInt());
+        // literals
+//        4;
+        // operations
+//        4 + 6;
+//        10;
+        rand.nextInt();
+        // method calls
+    }
+}
