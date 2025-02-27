4e1bcc3c1d33bd10a7fe8aaf99217adcd1e018bc
Joshua B. Gross <gross.joshua.b@gmail.com>
2025-02-26 16:04:37 -0800

    AM exam prep code

--- src/wk06/Sec1_Exam1Review.java
+++ src/wk06/Sec1_Exam1Review.java
@@ -2,6 +2,7 @@ package wk06;
 import java.util.*;
 public class Sec1_Exam1Review {
     public static void main(String[] args) {
+        System.out.println(Math.abs(Integer.MIN_VALUE + 1));
         int input = 1;
         Scanner in = new Scanner(System.in);
         while(input != 0) {
